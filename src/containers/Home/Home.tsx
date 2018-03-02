a2a4db511fa1319a3f21ee37919ed17d83a73a20
Olufemi Adeojo <oadeojo@prontoforms.com>
2018-03-02 16:44:36 -0500

    antd and modified variables

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-
+import { Button } from 'antd';
 export interface HomeProps {}
 
 export default class Home extends React.Component<HomeProps, {}> {
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <div>Home</div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
   }
 }
