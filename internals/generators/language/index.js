2b1c8b7e5656fc427b3087012bd56b5310c150fb
Femi <fadeojo@outlook.com>
2018-03-04 17:38:14 -0500

    language generator update

--- internals/generators/language/index.js
+++ internals/generators/language/index.js
@@ -33,58 +33,58 @@ module.exports = {
     const actions = [];
     actions.push({
       type: 'modify',
-      path: '../../app/i18n.js',
+      path: '../../src/i18n.ts',
       pattern: /('react-intl\/locale-data\/[a-z]+';\n)(?!.*'react-intl\/locale-data\/[a-z]+';)/g,
       templateFile: './language/intl-locale-data.hbs',
     });
     actions.push({
       type: 'modify',
-      path: '../../app/i18n.js',
+      path: '../../src/i18n.ts',
       pattern: /(\s+'[a-z]+',\n)(?!.*\s+'[a-z]+',)/g,
       templateFile: './language/app-locale.hbs',
     });
     actions.push({
       type: 'modify',
-      path: '../../app/i18n.js',
+      path: '../../src/i18n.ts',
       pattern: /(from\s'.\/translations\/[a-z]+.json';\n)(?!.*from\s'.\/translations\/[a-z]+.json';)/g,
       templateFile: './language/translation-messages.hbs',
     });
     actions.push({
       type: 'modify',
-      path: '../../app/i18n.js',
+      path: '../../src/i18n.ts',
       pattern: /(addLocaleData\([a-z]+LocaleData\);\n)(?!.*addLocaleData\([a-z]+LocaleData\);)/g,
       templateFile: './language/add-locale-data.hbs',
     });
     actions.push({
       type: 'modify',
-      path: '../../app/i18n.js',
+      path: '../../src/i18n.ts',
       pattern: /([a-z]+:\sformatTranslationMessages\('[a-z]+',\s[a-z]+TranslationMessages\),\n)(?!.*[a-z]+:\sformatTranslationMessages\('[a-z]+',\s[a-z]+TranslationMessages\),)/g,
       templateFile: './language/format-translation-messages.hbs',
     });
     actions.push({
       type: 'add',
-      path: '../../app/translations/{{language}}.json',
+      path: '../../src/translations/{{language}}.json',
       templateFile: './language/translations-json.hbs',
       abortOnFail: true,
     });
     actions.push({
       type: 'modify',
-      path: '../../app/app.js',
+      path: '../../src/App.tsx',
       pattern: /(import\('intl\/locale-data\/jsonp\/[a-z]+\.js'\),\n)(?!.*import\('intl\/locale-data\/jsonp\/[a-z]+\.js'\),)/g,
       templateFile: './language/polyfill-intl-locale.hbs',
     });
-    actions.push(
-      () => {
-        const cmd = 'npm run extract-intl';
-        exec(cmd, (err, result, stderr) => {
-          if (err || stderr) {
-            throw err || stderr;
-          }
-          process.stdout.write(result);
-        });
-        return 'modify translation messages';
-      }
-    );
+    // actions.push(
+    //   () => {
+    //     const cmd = 'npm run extract-intl';
+    //     exec(cmd, (err, result, stderr) => {
+    //       if (err || stderr) {
+    //         throw err || stderr;
+    //       }
+    //       process.stdout.write(result);
+    //     });
+    //     return 'modify translation messages';
+    //   }
+    // );
 
     return actions;
   },
